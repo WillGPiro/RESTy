fc5aad05efaa9a4bbb218d0cc3c988f257cdc9a5
Will G Piro <willgpiro@gmail.com>
2020-05-08 17:37:51 -0700

    add useEffect to set history anytime state changes

--- src/containers/FormControl.jsx
+++ src/containers/FormControl.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from '../components/Form/Form';
 import Response from '../components/Response/Response';
 import { makeRequest } from '../services/makeRequest';
 import HistoryList from '../components/History/HistoryList';
-
 
 const FormControl = () => {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState('GET');
   const [body, setBody] = useState('');
   const [response, setResponse] = useState({});
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState(localStorage.getItem('history') ? JSON.parse(localStorage.getItem('history')) : []);
+  
+  //Anytime history changes set local storage.
+  useEffect (() => {
+    localStorage.setItem('history', JSON.stringify(history));
+  }, [history]);
   
   //Handles user input based on name. 
   const handleChange = ({ target }) => {
@@ -22,7 +26,11 @@ const FormControl = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     makeRequest(url, method, body)
-      .then(json => setResponse(json));
+      .then(json => {
+        setResponse(json);
+        setHistory(prevHistory => [{ url, method }, ...prevHistory]);
+
+      });
   }; 
       
   return (
